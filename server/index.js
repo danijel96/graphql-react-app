587bfdb7879cfa285e50fce0b375108e2dcbaec8
danijel-jovanovic <dacajovanovic2012@gmail.com>
2022-10-18 13:20:42 +0200

    Prepare for deploy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const colors = require('colors');
-require('dotenv').config();
-const cors = require('cors');
-const schema = require('./schema/schema.js');
-const connectDB = require('./config/db');
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const cors = require("cors");
+const schema = require("./schema/schema.js");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
-const path = require('path');
+const path = require("path");
 
-const { graphqlHTTP } = require('express-graphql');
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
 
@@ -16,19 +16,23 @@ connectDB();
 app.use(cors());
 
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development',
+    graphiql: process.env.NODE_ENV === "development",
   })
 );
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+app.get("/", (req, res) => {
+  res.send("APP IS RUNNING");
+});
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-app.listen(port, console.log('Server listening on port ' + port));
+app.listen(port, console.log("Server listening on port " + port));
